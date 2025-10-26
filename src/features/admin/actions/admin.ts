ae79b3bd93f73e806ef57c9bda3183ad1a38296f
Jahidul Islam <jahidul.app@gmail.com>
2025-10-26 15:11:07 +0600

    feat: adding update and delete admin action

--- src/features/admin/actions/admin.ts
+++ src/features/admin/actions/admin.ts
@@ -3,6 +3,7 @@
 import { prisma } from "@/db/client";
 import { AdminSchemaType } from "@/schema/admin";
 import { hashPassword } from "@/utils/password";
+import { revalidatePath } from "next/cache";
 
 export const createAdmin = async (data: AdminSchemaType) => {
   try {
@@ -26,9 +27,98 @@ export const createAdmin = async (data: AdminSchemaType) => {
       },
     });
 
+    revalidatePath("/dashboard");
+    revalidatePath("/dashboard/admins");
+    revalidatePath("/dashboard/account");
+
     return {
       success: true,
       message: "New admin is successfully created",
+      data: {
+        admin: admin,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: (error as Error).message ?? "Something went wrong",
+    };
+  }
+};
+
+export const updateAdmin = async (id: string, data: AdminSchemaType) => {
+  try {
+    const user = await prisma.admin.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) throw new Error("Admin does not exist");
+
+    // check admin
+    const existingUser = await prisma.admin.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+
+    if (existingUser) {
+      throw new Error("Admin already exists with this username");
+    }
+
+    const admin = await prisma.admin.update({
+      where: { id },
+      data: {
+        full_name: data.full_name,
+        username: data.username,
+        password: await hashPassword(data.password),
+        role: "admin",
+      },
+    });
+
+    revalidatePath("/dashboard");
+    revalidatePath("/dashboard/admins");
+    revalidatePath("/dashboard/account");
+
+    return {
+      success: true,
+      message: "Admin is successfully updated",
+      data: {
+        admin: admin,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      success: false,
+      message: (error as Error).message ?? "Something went wrong",
+    };
+  }
+};
+
+export const deleteAdmin = async (id: string) => {
+  try {
+    const user = await prisma.admin.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!user) throw new Error("Admin does not exist");
+
+    const admin = await prisma.admin.delete({
+      where: { id },
+    });
+
+    revalidatePath("/dashboard");
+    revalidatePath("/dashboard/admins");
+    revalidatePath("/dashboard/account");
+
+    return {
+      success: true,
+      message: "Admin is deleted",
       data: {
         admin: admin,
       },
