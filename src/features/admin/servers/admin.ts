723606734ede21d217d5dd782ebdda9c28f3b284
Jahidul Islam <jahidul.app@gmail.com>
2025-10-26 15:12:52 +0600

    feat: adding get single and muti admin from server

---
+++ src/features/admin/servers/admin.ts
@@ -0,0 +1,55 @@
+import { prisma } from "@/db/client";
+import { DEFAULT_PAGE_SIZE } from "@/utils/data";
+
+export const getAdmins = async (page?: number, limit?: number) => {
+  const validatePage = page || 1;
+  const validateLimit = limit || DEFAULT_PAGE_SIZE;
+
+  try {
+    const admins = await prisma.admin.findMany({
+      skip: Number(validatePage - 1) * validateLimit,
+      take: validateLimit,
+    });
+
+    const count = await prisma.admin.count();
+
+    return {
+      success: true,
+      message: "Get admins successfull",
+      data: admins,
+      count: count,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      success: false,
+      message:
+        (error as Error).message.split("\n").pop() ?? "Something went wrong",
+      data: [],
+    };
+  }
+};
+
+export const getAdmin = async (id: string) => {
+  try {
+    const blog = await prisma.admin.findUnique({
+      where: { id },
+    });
+
+    return {
+      success: true,
+      message: "Get admin successfull",
+      data: blog,
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      success: false,
+      message:
+        (error as Error).message.split("\n").pop() ?? "Something went wrong",
+      data: null,
+    };
+  }
+};
