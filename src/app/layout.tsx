5ebe7c9e2b1f3a80c717f7f1928f9f7083cb2fcf
Jahidul Islam <jahidul.app@gmail.com>
2025-10-30 12:35:51 +0600

    chore: update runtime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import ProgressProviders from "@/providers/ProgressProvider";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
-import "@mdxeditor/editor/style.css"
-
+import "@mdxeditor/editor/style.css";
 
 export const metadata: Metadata = {
   title: "Calcium Calculator - Radiant Digital",
   description: "An app to help you maintain your calcium level with ease",
 };
+
+export const runtime = "nodejs";
 
 export const dynamic = "force-dynamic";
 
