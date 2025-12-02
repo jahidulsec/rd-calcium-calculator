11fe02ff08daad8b364403e25fae68848a4c7c0e
Jahidul Islam <jahidul.app@gmail.com>
2025-12-02 17:54:25 +0600

    chore: comparing two data of a food to show result

--- src/lib/calcium-api.ts
+++ src/lib/calcium-api.ts
@@ -3,7 +3,7 @@
 export async function getCalcium(foodName: string) {
   const apiKey = process.env.NEXT_PUBLIC_USDA_API_KEY;
   const res = await fetch(
-    `https://api.nal.usda.gov/fdc/v1/foods/search?query=${foodName}&api_key=${apiKey}&pageSize=1`,
+    `https://api.nal.usda.gov/fdc/v1/foods/search?query=${foodName}&api_key=${apiKey}&pageSize=2`,
   );
 
   if (res.status === 404) throw new Error("Check the food name spelling for better result")
@@ -12,9 +12,18 @@ export async function getCalcium(foodName: string) {
 
   const data = await res.json();
 
-  const nutrients = data.foods[0]?.foodNutrients;
-  const calcium = nutrients?.find((n: any) => n.nutrientName === "Calcium, Ca");
-  return calcium ? calcium.value : null;
+  const nutrients1 = data.foods[0]?.foodNutrients;
+  const nutrients2 = data.foods?.[1]?.foodNutrients;
+  const calcium1 = nutrients1?.find((n: any) => n.nutrientName === "Calcium, Ca");
+  const calcium2 = nutrients2?.find((n: any) => n.nutrientName === "Calcium, Ca");
+
+  let value = calcium1.value
+
+  if (calcium2 && calcium2.value > calcium1.value) {
+    value = calcium2.value
+  }
+
+  return value ? value : null;
 }
 
 export const searchFood = async (search: string) => {
